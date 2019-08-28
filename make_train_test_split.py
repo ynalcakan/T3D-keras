01bcf35cf7ffa720b07e9fa3a37cc3a25b63a385
ynalcakan <yagiznalcakan@gmail.com>
2019-08-28 17:11:03 +0300

    split file updated.

--- make_train_test_split.py
+++ make_train_test_split.py
@@ -2,19 +2,18 @@ from random import shuffle
 import csv
 import glob
 
-action_classes = ['HorseRace', 'YoYo', 'SoccerJuggling', 'MilitaryParade', 'BaseballPitch', 'JugglingBalls', 'BenchPress', 'Biking', 'VolleyballSpiking', 'PlayingGuitar', 'ThrowDiscus', 'SalsaSpin', 'PlayingPiano', 'PoleVault', 'Mixing', 'PlayingViolin', 'CleanAndJerk', 'Basketball', 'HulaHoop', 'JumpingJack', 'RopeClimbing', 'GolfSwing', 'PizzaTossing', 'Fencing', 'TrampolineJumping', 'Billiards', 'Nunchucks', 'PommelHorse', 'SkateBoarding', 'HorseRiding', 'TennisSwing', 'TaiChi', 'Diving', 'Drumming', 'WalkingWithDog', 'Skijet', 'Lunges', 'Rowing', 'PlayingTabla', 'Punch', 'BreastStroke', 'RockClimbingIndoor', 'JavelinThrow', 'PushUps', 'Kayaking', 'Skiing', 'Swing', 'PullUps', 'JumpRope', 'HighJump']
-print(len(set(action_classes)))
+action_classes = ['Cut-in', 'LanePass']
+
+
 def create_csvs():
     train = []
     test = []
-    total_train = []
-    total_test = []
 
     for myclass, directory in enumerate(action_classes):
-        for filename in glob.glob('UCF50/{}/*.avi'.format(directory)):
+        for filename in glob.glob('Data/{}/*.avi'.format(directory)):
             group = ((filename.split('/')[-1]).split('.')[0]).split('_')[-2]
 
-            if group in ['g01','g02','g02','g04', 'g05']:
+            if group in ['g01', 'g02', 'g02', 'g04', 'g05']:
                 test.append([filename, myclass, directory])
             else:
                 train.append([filename, myclass, directory])
@@ -26,13 +25,13 @@ def create_csvs():
 
     with open('train.csv', 'w') as csvfile:
         mywriter = csv.writer(csvfile)
-        mywriter.writerow(['path', 'class', 'sport'])
+        mywriter.writerow(['path', 'class', 'action'])
         mywriter.writerows(train)
         print('Training CSV file created successfully')
 
     with open('test.csv', 'w') as csvfile:
         mywriter = csv.writer(csvfile)
-        mywriter.writerow(['path', 'class', 'sport'])
+        mywriter.writerow(['path', 'class', 'action'])
         mywriter.writerows(test)
         print('Testing CSV file created successfully')
 
